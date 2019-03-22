d8af838d8d66c5155ef9b8b92edbd397d4f75463
Roman Pavlushchenko <roman.pavlushchenko@gmail.com>
2019-03-22 05:55:49 +0000

    Start tracking config.phps

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/cherry/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/cherry/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/cherry/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/cherry/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/cherry/admin/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/cherry/system/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/cherry/admin/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/cherry/admin/view/template/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/cherry/system/config/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/cherry/image/');
+define('DIR_CACHE', '/opt/lampp/htdocs/cherry/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/cherry/system/storage/download/');
+define('DIR_LOGS', '/opt/lampp/htdocs/cherry/system/storage/logs/');
+define('DIR_MODIFICATION', '/opt/lampp/htdocs/cherry/system/storage/modification/');
+define('DIR_UPLOAD', '/opt/lampp/htdocs/cherry/system/storage/upload/');
+define('DIR_CATALOG', '/opt/lampp/htdocs/cherry/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cherry');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
