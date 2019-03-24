4327ba79db35f707c8e0180472d15566ec010eb9
Roman Pavlushchenko <roman.pavlushchenko@gmail.com>
2019-03-24 03:40:03 +0000

    Add 'hermes' option to the cart.

---
+++ catalog/language/english/shipping/hermes.php
@@ -0,0 +1,7 @@
+<?php
+// Text
+$_['text_title']       = 'Hermes';
+$_['text_description'] = 'Hermes shipping rate';
+$_['text_weight']      = 'Weight:';
+$_['text_insurance']   = 'Insured upto:';
+$_['text_time']        = 'Estimated Time: Within 3-5 days (Tracked)    ';
