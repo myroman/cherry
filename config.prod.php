af2bbcc30d551c3fe304358c652ae5ceab42b719
Roman Pavlushchenko <roman.pavlushchenko@gmail.com>
2019-05-21 03:42:21 +0000

    Add templates for configs for local and prod

---
+++ config.prod.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/u0723758/data/www/xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/catalog/');
+define('DIR_SYSTEM', '/var/www/u0723758/data/www/xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/system/');
+define('DIR_LANGUAGE', '/var/www/u0723758/data/www/xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/u0723758/data/www/xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/u0723758/data/www/xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/system/config/');
+define('DIR_IMAGE', '/var/www/u0723758/data/www/xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/image/');
+define('DIR_CACHE', '/var/www/u0723758/data/www/xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/u0723758/data/www/xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/system/storage/download/');
+define('DIR_LOGS', '/var/www/u0723758/data/www/xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/u0723758/data/www/xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/u0723758/data/www/xn--b1abgaoctmgb1cbl4eg5dxc.xn--p1acf/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'u0723758_default');
+define('DB_PASSWORD', 'c!7jlwcA');
+define('DB_DATABASE', 'u0723758_default');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
